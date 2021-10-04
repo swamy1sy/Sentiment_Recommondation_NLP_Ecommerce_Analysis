5957604ba295d5adff95e883cf0974b4be4fdaaa
swamy1sy <swamy1sy@gmail.com>
2021-10-04 18:29:09 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,30 @@
+# Importing the libraries
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import pickle
+import requests
+import json
+
+# Importing the dataset
+dataset = pd.read_csv('sample30.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Fitting Logistic Regression to the Training set
+regressor = LogisticRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+# Saving model to disk
+pickle.dump(regressor, open('model.pkl','wb'))
+
+# Loading model to compare the results
+model = pickle.load(open('model.pkl','rb'))
+print(model.predict([[:5]]))
